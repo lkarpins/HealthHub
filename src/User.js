69e3354df6bb1fea63ba238686e555729d41ae7f
Rachel Allen <rallenco13@gmail.com>
2022-05-16 16:40:26 -0600

    set up User class and test

---
+++ src/User.js
@@ -0,0 +1,13 @@
+// `new User(userData);`
+//
+// - A `User` represents a single user
+// - It should have a parameter to take in a `userData` object
+// - Each user holds on to the user properties from the data file
+// - Should have a method to:
+//     - Return a user’s first name only
+
+class User {
+  constructor() {}
+}
+
+export default User;
