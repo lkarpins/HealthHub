29939c407c962e5279468f79d100069f1d87d24a
Rachel Allen <rallenco13@gmail.com>
2022-05-16 17:05:40 -0600

    added name property and test

--- test/User-test.js
+++ test/User-test.js
@@ -4,7 +4,7 @@ import User from "../src/User";
 describe("User", () => {
   let user;
   beforeEach(function() {
-    user = {
+    user = new User({
       id: 1,
       name: "Luisa Hane",
       address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
@@ -12,7 +12,7 @@ describe("User", () => {
       strideLength: 4.3,
       dailyStepGoal: 10000,
       friends: [16, 4, 8]
-    };
+    });
   });
 
   it("should be a function", function() {
@@ -20,12 +20,14 @@ describe("User", () => {
   });
 
   it("should be an instance of User", function() {
-    const user = new User();
     expect(user).to.be.an.instanceof(User);
   });
 
   it("should be able to store a user id", function() {
-    const newUser = new User(user);
-    expect(newUser.id).to.equal(user.id);
+    expect(user.id).to.equal(1);
+  });
+
+  it("should be able to store a user name", function() {
+    expect(user.name).to.equal("Luisa Hane");
   });
 });
