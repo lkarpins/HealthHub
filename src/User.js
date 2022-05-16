d4045d6dae4d5c5f28335c6627d4647cc27eefc2
Rachel Allen <rallenco13@gmail.com>
2022-05-16 16:58:25 -0600

    added id property and test to User class

--- src/User.js
+++ src/User.js
@@ -7,7 +7,15 @@
 //     - Return a user’s first name only
 
 class User {
-  constructor() {}
+  constructor(userData) {
+    this.id = userData.id,
+    this.name = userData.name,
+    this.address = userData.address,
+    this.email = userData.email,
+    this.strideLength = userData.strideLength,
+    this.dailyStepGoal = userData.dailyStepGoal,
+    this.friends = userData.friends,
+  };
 }
 
 export default User;
--- test/User-test.js
+++ test/User-test.js
@@ -2,6 +2,19 @@ import { expect } from "chai";
 import User from "../src/User";
 
 describe("User", () => {
+  let user;
+  beforeEach(function() {
+    user = {
+      id: 1,
+      name: "Luisa Hane",
+      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      email: "Diana.Hayes1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8]
+    };
+  });
+
   it("should be a function", function() {
     expect(User).to.be.a("function");
   });
@@ -9,5 +22,10 @@ describe("User", () => {
   it("should be an instance of User", function() {
     const user = new User();
     expect(user).to.be.an.instanceof(User);
+  });
+
+  it("should be able to store a user id", function() {
+    const newUser = new User(user);
+    expect(newUser.id).to.equal(user.id);
   });
 });
