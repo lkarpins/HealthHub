076e10b38544609db93a185ee54c1ebee9c01c84
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-05-18 14:00:51 -0400

    added tests for second user

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -3,42 +3,48 @@ import Hydration from "../src/Hydration";
 const sampleHydrationData = require("../src/data/sample-hydration-data");
 
 describe("Hydration", () => {
-  let userHydration;
+  let user1Hydration;
+  let user2Hydration;
   beforeEach(() => {
-    userHydration = new Hydration(sampleHydrationData[0]);
+    user1Hydration = new Hydration(sampleHydrationData[0]);
+    user2Hydration = new Hydration(sampleHydrationData[1]);
   });
   it("should be a function", () => {
     expect(Hydration).to.be.a("function");
   });
 
   it("should be an instance of Hydration", () => {
-    expect(userHydration).to.be.an.instanceof(Hydration);
+    expect(user1Hydration).to.be.an.instanceof(Hydration);
+    expect(user2Hydration).to.be.an.instanceof(Hydration);
   });
 
   it("should be able to store a user id", () => {
-    expect(userHydration.userID).to.equal(1);
+    expect(user1Hydration.userID).to.equal(1);
+    expect(user2Hydration.userID).to.equal(2);
   });
 
   it("should be able to store a date", () => {
-    expect(userHydration.date).to.equal("2019/06/15");
+    expect(user1Hydration.date).to.equal("2019/06/15");
+    expect(user2Hydration.date).to.equal("2019/06/15");
   });
 
   it("should be able to store ounces of fluid consumed", () => {
-    expect(userHydration.numOunces).to.equal(37);
+    expect(user1Hydration.numOunces).to.equal(37);
+    expect(user2Hydration.numOunces).to.equal(75);
   });
 
   it.skip("should be able to return the average fluid ounces consumed per day for all time", () => {
-    const avgHydrationPerDay = userHydration.returnAvgHydrationPerDay();
+    const avgHydrationPerDay = user1Hydration.returnAvgHydrationPerDay();
     expect(avgHydrationPerDay).to.equal();
   });
 
   it.skip("should be able to return how many ounces a user consumed for a specific day", () => {
-    const consumedOuncesPerDay = userHydration.returnDailyOunces();
+    const consumedOuncesPerDay = user1Hydration.returnDailyOunces();
     expect(consumedOuncesPerDay).to.equal();
   });
 
   it.skip("should be able to return the amount of ounces consumed each day over the course of a week", () => {
-    const weeklyOuncesPerDay = userHydration.returnWeeklyOunces();
+    const weeklyOuncesPerDay = user1Hydration.returnWeeklyOunces();
     expect(weeklyOuncesPerDay).to.equal();
   });
 });
