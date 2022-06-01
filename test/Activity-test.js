756fdfa94cad4ad7a06776799a79f348574be626
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-06-01 11:22:49 -0600

    feat: add test for returnDailyMilesWalked method for Activity class

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -42,5 +42,25 @@ describe("Activity", () => {
     expect(user1Activity.flightsOfStairs).to.equal(6);
     expect(user2Activity.flightsOfStairs).to.equal(45);
   });
+  
+  it("should be able to return how many miles a user has walked based on their number of steps", () => {
+    const milesWalkedPerDay1 = user1Activity.returnDailyMilesWalked("2019/06/16");
+    const milesWalkedPerDay2 = user2Activity.returnDailyMilesWalked("2019/06/16");
+    expect(milesWalkedPerDay1).to.equal();
+    expect(milesWalkedPerDay2).to.equal();
+  });
+// -For a user, (identified by their userID) how many minutes were they active for a given day (specified by a date)?
 
+// -For a user, how many minutes active did they average for a given week (7 days)?
+
+// -For a user, did they reach their step goal for a given day (specified by a date)?
+
+// -For a user, find all the days where they exceeded their step goal
+
+// -For a user, find their all-time stair climbing record
+
+// -For all users, what is the average number of:
+// stairs climbed for a specified date
+// steps taken for a specific date
+// minutes active for a specific date
 });
