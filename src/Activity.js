a5d2afdc6e203bb6435ec8fc80ca4cd652c33c4d
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-06-01 14:00:46 -0400

    Added returnDailyMilesWalked naming

--- src/Activity.js
+++ src/Activity.js
@@ -21,5 +21,12 @@ class Activity {
     return userActive;
   };
 
-
+  // -For a specific day (specified by a date), return the miles a user has walked based on their number of steps (use their strideLength to help calculate this)
+  returnDailyMilesWalked = date => {
+    let dailyMiles = this.userActivityData.find(entry => {
+      return entry.date === date;
+    });
+    return Math.round(dailyMiles.numSteps * user.strideLength) / 5280;
+  };
+}
 export default Activity;
